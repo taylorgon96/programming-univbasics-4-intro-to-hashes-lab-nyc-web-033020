e38170dc91671715824475b30090a7dbee08d0c5
Taylor Gonzalez <taylorgon@gmail.com>
2020-02-28 21:36:00 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,12 +23,10 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-
-  hash.update(hash) { |key , 1 | value }
- 
- if hash[key]
+  
+  if hash[key]
     hash[key] += 1
-else
+  else
     hash[key] = 1
 end
 end
