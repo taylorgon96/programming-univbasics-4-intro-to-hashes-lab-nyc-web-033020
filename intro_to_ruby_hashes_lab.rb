caddbc483aa698d9a388a03e6d8ff6004f3a0b8f
Taylor Gonzalez <taylorgon@gmail.com>
2020-02-28 21:34:08 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,12 +23,12 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-  value = 1 
+
   hash.update(hash) { |key , value| value }
  
  if hash[key]
     hash[key] += 1
 else
-  shipping_manifest["top hat"] = 1
+    hash[key] = 1
 end
 end
